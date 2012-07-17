cc422391675aa0e760f761bddd6278eed9bbaa5f
NunoRodrigues <gpulga@gmail.com>
2012-07-17 12:48:56 +0100

    Merge branch 'master' into HEAD

---
+++ Drivers/PatrocinioAdminDriver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.Drivers;
+using Patrocinadores.Models;
+
+namespace Patrocinadores.Drivers
+{
+    public class PatrocinioAdminDriver : ContentPartDriver<PatrocinioAdminPart>
+    {
+        protected override DriverResult Display(PatrocinioAdminPart part, string displayType, dynamic shapeHelper)
+        {
+            return ContentShape("Parts_PatrocinioAdmin", () => shapeHelper.Parts_PatrocinioAdmin(Width: part.Width, Height: part.Height));
+        }
+
+        //GET
+        protected override DriverResult Editor(PatrocinioAdminPart part, dynamic shapeHelper)
+        {
+            return ContentShape("Parts_PatrocinioAdmin_Edit", () => shapeHelper.EditorTemplate(TemplateName: "Parts/PatrocinioAdmin", Model: part, Prefix: Prefix));
+        }
+
+        //POST
+        protected override DriverResult Editor(PatrocinioAdminPart part, IUpdateModel updater, dynamic shapeHelper)
+        {
+            updater.TryUpdateModel(part, Prefix, null, null);
+            return Editor(part, shapeHelper);
+        }
+    }
+}
---
+++ Migrations.cs
@@ -0,0 +1,69 @@
+﻿using System.Data;
+
+using Orchard.ContentManagement.MetaData;
+using Orchard.Core.Contents.Extensions;
+using Orchard.Data.Migration;
+
+using Patrocinadores.Models;
+
+namespace Patrocinadores
+{
+    /// <summary>
+    /// Repositorio de carregamento de Configurações, Estruturas de Dados, etc.
+    /// </summary>
+    public class Migrations : DataMigrationImpl
+    {
+        /// <summary>
+        ///     Primeiro Metodo que é chamado quando se instala o Modulo.
+        ///     Os consequentes metodos são chamados "UpdateFromX" em que X é um Numero e essa mesma função retorna sempre "X + 1"
+        /// </summary>
+        /// <returns>
+        ///     Numero de Versão, sendo este o "Create", é sempre "1".
+        ///     Os Restantes Metodos serão sempre "UpdateFromX"
+        ///     Para fazer reset Manual da Versão do Modulo basta alterar o registo correspondente na Tabela "*_Orchard_Framework_DataMigrationRecord"
+        /// </returns>
+        public int Create()
+        {
+            // Creating table MapRecord
+            SchemaBuilder.CreateTable("PatrocinioAdminRecord", table => table
+                .ContentPartRecord()
+                .Column("Width", DbType.Int32)
+                .Column("Height", DbType.Int32)
+            );
+
+            ContentDefinitionManager.AlterPartDefinition(typeof(PatrocinioAdminPart).Name, cfg => cfg.Attachable());
+            
+            return 1;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>Tem de ser "2" porque a função se chama "UpdateFrom1"</returns>
+        public int UpdateFrom1()
+        {
+         // Create a new widget content type with our map
+            ContentDefinitionManager.AlterTypeDefinition("PatrocinioAdminWidget", cfg => cfg
+                .WithPart("PatrocinioAdminPart")
+                .WithPart("WidgetPart")
+                .WithPart("CommonPart")
+                .WithSetting("Stereotype", "Widget"));
+         
+            return 2;
+        }
+        
+        /*
+        public int UpdateFrom2() {
+            return 3;
+        }
+        
+        public int UpdateFrom3() {
+            return 4;
+        }
+		
+        public int UpdateFrom4() {
+            return 5;
+        }
+        */
+    }
+}
