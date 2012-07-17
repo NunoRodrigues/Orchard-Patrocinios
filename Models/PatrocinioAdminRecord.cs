759cdf7496d26d297631257fa8a98d72a77de937
NunoRodrigues <gpulga@gmail.com>
2012-07-17 12:36:02 +0100

    First Commit

---
+++ Models/PatrocinioAdminRecord.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.Records;
+
+namespace Patrocinadores.Models
+{
+    public class PatrocinioAdminRecord : ContentPartRecord
+    {
+        public virtual int Width { get; set; }
+        public virtual int Height { get; set; }
+    }
+
+    public class PatrocinioAdminPart : ContentPart<PatrocinioAdminRecord>
+    {
+        [Required]
+        public int Width
+        {
+            get { return Record.Width; }
+            set { Record.Width = value; }
+        }
+
+        [Required]
+        public int Height
+        {
+            get { return Record.Height; }
+            set { Record.Height = value; }
+        }
+    }
+}
